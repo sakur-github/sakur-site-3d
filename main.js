a4625d981b8508ab331decf4a3279786ac80084e
oliverlevay <oliver.levay@hotmail.com>
2021-12-14 03:50:20 +0100

    lerp

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import "./style.css";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js";
+import { Vector3 } from "three";
 const isDesktop = window.innerWidth > 768;
 let mixer = new THREE.AnimationMixer();
 const clock = new THREE.Clock();
@@ -20,7 +21,7 @@ const camera = new THREE.PerspectiveCamera(
 );
 const startingX = 0;
 const startingY = 1;
-const startingZ = 4;
+const startingZ = 2;
 if (!characterLoaded) {
   window.scrollTo({ top: 0 });
   camera.position.set(startingX, startingY, startingZ);
@@ -35,32 +36,23 @@ window.addEventListener("scroll", () => {
   const oldScroll = newScroll;
   newScroll = window.scrollY;
   const scrollDelta = oldScroll - newScroll;
-  if (scrollY <= 200) {
+  if (scrollY <= 500) {
     //camera.translateZ(scrollDelta * 0.016);
-    camera.position.set(startingX, startingY, startingZ + scrollY * -0.015);
-    camera.rotation.set(0, 0, 0);
-  } else if (scrollY <= 400) {
-    camera.position.set(startingX + (scrollY - 200) * 0.0005, startingY, 1);
-    camera.rotation.set(0, 0, 0);
-  }
-  //} else if (scrollY <= 500) {
-  else if (scrollY <= 550) {
-    camera.position.set(0.1 + (scrollY - 400) * 0.009, 1, 1);
-    camera.rotation.set(0, (scrollY - 400) * 0.006, 0);
-  } else if (scrollY <= 835) {
-    camera.position.set(1.45, 1, 1 + (scrollY - 550) * -0.01);
-    //camera.rotation.set(0, 0.9, 0);
-    camera.rotation.set(0, 0.9 + (scrollY - 550) * 0.0055, 0);
-    console.log(camera.position);
-    console.log(scrollY);
-  } else if (scrollY <= 968) {
-    camera.rotation.set(0, 2.5, 0);
-    camera.translateX(scrollDelta * -0.000019);
-    camera.translateZ(scrollDelta * 0.016);
+    if (scrollDelta < 0) {
+      camera.position.lerp(new Vector3(startingX + 0.1, startingY, 1), 0.05);
+    } else if (scrollDelta > 0) {
+      camera.position.lerp(new Vector3(startingX, startingY, startingZ), 0.05);
+    }
   } else {
-    console.log(camera.position);
-    console.log(scrollY);
-  }
+  } /* else if (scrollY > 300) {
+    if (scrollDelta < 0) {
+      //camera.rotateY(0.001);
+      camera.position.lerp(new Vector3(startingX + 1, startingY, -1), 0.01);
+    } else if (scrollDelta > 0) {
+      camera.position.lerp(new Vector3(startingX, startingY, 0.5), 0.1);
+      // camera.rotateY(-0.001);
+    }
+  } */
 });
 
 const canvas = document.querySelector("#bg");
